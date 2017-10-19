17fdb0fb9cbfec346ed3bea57f65862140484504
WiLLLLL683 <tulohonov_aleks@mail.ru>
2017-10-20 07:01:13 +0800

    New killing pit!

---
+++ Project RockMan/Assets/Scripts/Kill_Respan.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill_Respan : MonoBehaviour {
+
+	public Rigidbody2D gamePlayer;
+	private bool freeseRotation;
+
+	[SerializeField]Transform spawnPoint;
+
+	IEnumerator OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.transform.CompareTag ("Player"))
+			gamePlayer.freezeRotation = false; 
+			yield return new WaitForSeconds (2f);
+			col.transform.position = spawnPoint.position;
+			gamePlayer.transform.rotation = spawnPoint.rotation;
+			gamePlayer.freezeRotation = true;
+			
+	}
+
+
+}
+
